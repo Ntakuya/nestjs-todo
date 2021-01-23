0d68278100be1d3d5bece1eb809533802dbf704f
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro.local>
2021-01-23 19:33:43 +0900

    feat(database) add database connection

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,12 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
+    type: 'mysql',
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    entities: [],
   },
 });
